eb1b00111fdc2a2fdd45a88448314f5e524c1d4a
ksh <kangshqwert@naver.com>
2023-04-11 14:32:04 +0900

    [기능추가]: 레이아웃 컴포넌트트 추가, 글로벌 스타일, 유틸 스타일 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Layout from '~/components/Layout';
+
+import '~/styles/global.css';
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout || (page => <Layout home={false}>{page}</Layout>);
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
---
+++ src/pages/posts/first-post.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>첫번째 글</title>
+      </Head>
+      <h1>first-post</h1>
+    </>
+  );
+}
